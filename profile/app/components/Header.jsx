49a8b60faf5e7b0e837a11c4605ce05f4ce7d111
Vo Dinh Ngoc Binh <minhtu577@gmail.com>
2025-07-25 01:53:35 +0700

    update Work + Services

--- profile/app/components/Header.jsx
+++ profile/app/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
       </div>
       <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
-        Hi! I'm Vo Dinh Ngoc Binh{" "}
+        Hi! I'm Võ Đình Ngọc Bình{" "}
         <Image src={assets.hand_icon} alt="" className="w-6" />
       </h3>
       <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
--- profile/app/layout.js
+++ profile/app/layout.js
@@ -1,5 +1,7 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
+
+// Import fonts
 const outfit = Outfit({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -17,9 +19,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+        className={`scroll-smooth ${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
       >
         {children}
       </body>
