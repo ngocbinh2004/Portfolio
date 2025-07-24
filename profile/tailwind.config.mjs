39d120c533dc78700f42cfd015913524a122352f
Vo Dinh Ngoc Binh <minhtu577@gmail.com>
2025-07-24 16:24:51 +0700

    update About

---
+++ profile/tailwind.config.mjs
@@ -0,0 +1,31 @@
+import { join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    join(__dirname, "app/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "components/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "pages/**/*.{js,ts,jsx,tsx,mdx}"),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#2a004a",
+        darkTheme: "#11001F",
+      },
+      boxShadow: {
+        black: "4px 4px 0 #000",
+        white: "4px 4px 0 #fff",
+      },
+    },
+  },
+  plugins: [],
+};
